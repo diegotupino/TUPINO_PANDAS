90903a0a75ddbe27d4cf22543631b32dcaf19aba
diegotupino <57567290+diegotupino@users.noreply.github.com>
2019-11-23 21:19:47 +0800

    Add files via upload

---
+++ Pandas_Problem2_TUPINO.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df = pd.read_csv('cars.csv')
+print(df.iloc[0:5,0::2])
+
+print(df.loc[0,'Model':])
+
+print(df.loc[[23],['Model','cyl','gear']])
+
+print(df.loc[[1,18,28],['Model','cyl','gear']])
